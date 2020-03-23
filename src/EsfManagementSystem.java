5d7994164f17ef52b0015e5c15fa2446874e3722
Administrator <Administrator@WIN-BPG8R9JIL9E>
2020-03-23 12:20:05 +0900

    Create a menu

--- src/EsfManagementSystem.java
+++ src/EsfManagementSystem.java
@@ -1,13 +1,12 @@
-public class EsfManagementSystem {
 
+public class EsfManagementSystem {
 	public static void main(String[] args) {
 		System.out.println("*** Esf Management System Menu ***");
 		System.out.println(" 1. Add member");
 		System.out.println(" 2. Delate member");
 		System.out.println(" 3. Edit member");
 		System.out.println(" 4. Veiw member");
-		System.out.println(" 	5. Exit");
+		System.out.println(" 5. Exit");
 		System.out.println("Select one number between 1 - 5");
 	}
-
 }
